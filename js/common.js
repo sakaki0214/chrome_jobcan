c310ce1d8a9e2adf744c76030f6f8e7fd2d58ac4
榊原美穂 <sakaki@sakakibaramiho-no-MacBook-Pro.local>
2016-04-11 09:33:24 +0900

    headerまわり

--- js/common.js
+++ js/common.js
@@ -1,9 +1,21 @@
 $(function(){
+  //common
   $body = $('body');
   $header = $('#header');
   $headerInner = $('#header #header-inner');
+  $logo = $('#header h1 a');
+  $logoutContents = $('#login-contents');
+  $headerName = $('#login-contents span');
+  $headerNameLink = $('#login-contents span a');
+  $btnLogout = $('#login-contents .btn-small');
+  $headerTd = $('#header td');
   $footer = $('#all-footer');
+  $navBar = $('.navbar');
+  $navBarInner = $('.navbar-inner');
+  $nav = $('.nav');
 
+
+  //top
   $loginBlock = $('#login-block');
   $selectLang = $('#select_lang');
   $loginBlock = $('#login-block');
@@ -20,6 +32,56 @@ $(function(){
   $footer.css({
 
   });
+  $logo.text('JOBCAN').css({
+    'text-decoration':'none',
+    'color':'rgb(3, 165, 255)',
+    'font-family':'Avenir, sans-serif',
+    'font-size':'36px'
+  });
+  $headerName.css({
+    'color':'#fff',
+    'padding':'1px 0 5px',
+    'display':'inline-block'
+  });
+  $headerNameLink.css({
+    'color':'#fff'
+  })
+  $btnLogout.css({
+    'height':'auto',
+    'font-family':'Avenir, sans-serif',
+    'font-size':'10px',
+    'font-weight':'lighter',
+    'line-height':'1',
+    'padding':'5px 10px',
+    'letter-spacing':'.1em',
+    'background':'none',
+    'border':'1px solid #fff',
+    'color':'#fff',
+    'text-shadow':'0 0 0 #000'
+  }).mouseenter(function(){
+    $(this).css({
+      'color':'#ccc',
+      'border-color':'#ccc'
+    });
+  }).mouseleave(function(){
+    $(this).css({
+      'color':'#fff',
+      'border-color':'#fff'
+    });
+  });
+  $headerTd.css({
+    'vertical-align':'middle'
+  });
+  $logoutContents.css({
+    'padding-top':'10px'
+  });
+  $navBarInner.css({
+    'background':'#fff',
+    'border':'none'
+  });
+  $nav.css({
+    'width':'1280px'
+  })
 
   //top page
   $selectLang.css({
